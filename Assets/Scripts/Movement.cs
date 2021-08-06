cd7a96231f4c311b832934b6dbf0033250420030
Aura <edvin.garibovic@gmail.com>
2021-08-06 18:24:07 +0200

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //public isGrounded;
+    private float movementspd = 40f;
+    public CharacterController2D controller2D;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       horizontalMove = Input.GetAxisRaw("Horizontal") * movementspd; // A = -1 and D = 1
+        if (Input.GetButtonDown("Jump")) { 
+            jump = true;
+            //Debug.Log("Jumped lol ");
+        }
+        else jump = false;
+
+    }
+
+    void FixedUpdate(){
+        //Movement of the character (in the Character controller script)
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+    }
+}
